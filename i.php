d172c622919f0b5b969be84b2d7521876190eb6b
Arijul007 <148209816+Arijul007@users.noreply.github.com>
2024-01-27 19:29:49 +0530

    Add files via upload

---
+++ i.php
@@ -0,0 +1,26 @@
+<?php
+    $login = false;
+    $showError = false;
+    $conn = new mysqli('localhost','root', '', 'details');
+    if($_SERVER["REQUEST_METHOD"] == "POST")
+    {
+        $Username = $_POST["Username"];
+        $password = $_POST["password"];
+        $sql = "Select * from c_details where Username='$Username' AND password='$password'";
+        $result = mysqli_query($conn, $sql);
+        $num = mysqli_num_rows($result);
+        if ($num == 1)
+        {
+            $login = true;
+            session_start();
+            $_SESSION['loggedin'] = true;
+            $_SESSION['username'] = $username;
+            header("location: index.php");
+        }
+        else
+        {
+            header("location: logic.php");
+            
+        }
+    }
+?>
---
+++ s.php
@@ -0,0 +1,42 @@
+
+
+
+<?php
+
+   
+
+    $Username = $_POST['Username'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+     //Database connection
+
+    $conn = new mysqli('localhost','root', '', 'details');
+
+    if($conn->connect_error) 
+    {
+
+        die('Connection Failed:' .$conn->connect_error);
+
+    }  
+    else
+    {
+
+        $stmt = $conn->prepare("insert into c_details(Username, email, password)values(?, ?, ?)");
+
+        $stmt->bind_param("sss", $Username, $email, $password);
+
+        $stmt->execute();
+
+        echo '<div class="alert alert-success alert-dismissible fade show" role="alert"> <strong>Success!</strong>
+                    Your account is now created and you can login 
+                    <button type="button" class="close" data-dismiss="alert" aria-label="Close"> <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>';
+                
+        
+
+        $stmt->close();
+
+        $conn->close();
+    }
+?>
