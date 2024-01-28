a092574371400130ee8057b3bd42454c8818dd48
ritwickrajmakhal <ritwickrajmakhal11@gmail.com>
2024-01-28 12:12:26 +0530

    Add login and sign up functionality

--- login.php
+++ login.php
@@ -1,11 +1,20 @@
+<?php
+session_start();
+if (isset($_SESSION['loggedin']) || $_SESSION['loggedin'] == true) {
+    header("location: index.php");
+    exit;
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <link rel="icon" type="image/icon" href="images/88.gif">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
-    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" type="text/css" href="css/login_style.css">
     <title>Login page</title>
 </head>
 
@@ -38,11 +47,11 @@
             <form action="s.php" method="post">
                 <h2>Sign Up</h2>
                 <div class="input-group">
-                    <input type="text"  id="Username" name="Username" required>
+                    <input type="text" id="Username" name="Username" required>
                     <label for="Username">Username</label>
                 </div>
                 <div class="input-group">
-                    <input type="email"  id="email" name="email" required>
+                    <input type="email" id="email" name="email" required>
                     <label for="email">Email</label>
                 </div>
                 <div class="input-group">
@@ -62,6 +71,7 @@
 
 
 
-    <script src="script.js"></script>
+    <script src="js/login_script.js"></script>
 </body>
+
 </html>
--- s.php
+++ s.php
@@ -1,42 +1,38 @@
-
-
-
 <?php
+require_once __DIR__ . '/vendor/autoload.php'; // Load Composer's autoloader
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
 
-   
 
-    $Username = $_POST['Username'];
-    $email = $_POST['email'];
-    $password = $_POST['password'];
-     //Database connection
+$Username = $_POST['Username'];
+$email = $_POST['email'];
+$password = $_POST['password'];
+//Database connection
 
-    $conn = new mysqli('localhost','root', '', 'details');
+$conn = new mysqli($_ENV['DB_HOST'], $_ENV['DB_USER'], $_ENV['DB_PASSWORD'], $_ENV['DB_NAME']);
 
-    if($conn->connect_error) 
-    {
+if ($conn->connect_error) {
 
-        die('Connection Failed:' .$conn->connect_error);
+    die('Connection Failed:' . $conn->connect_error);
 
-    }  
-    else
-    {
+} else {
 
-        $stmt = $conn->prepare("insert into c_details(Username, email, password)values(?, ?, ?)");
+    $stmt = $conn->prepare("insert into c_details(Username, email, password)values(?, ?, ?)");
 
-        $stmt->bind_param("sss", $Username, $email, $password);
+    $stmt->bind_param("sss", $Username, $email, $password);
 
-        $stmt->execute();
+    $stmt->execute();
 
-        echo '<div class="alert alert-success alert-dismissible fade show" role="alert"> <strong>Success!</strong>
+    echo '<div class="alert alert-success alert-dismissible fade show" role="alert"> <strong>Success!</strong>
                     Your account is now created and you can login 
                     <button type="button" class="close" data-dismiss="alert" aria-label="Close"> <span aria-hidden="true">&times;</span>
                     </button>
                 </div>';
-                
-        
 
-        $stmt->close();
 
-        $conn->close();
-    }
+
+    $stmt->close();
+
+    $conn->close();
+}
 ?>
