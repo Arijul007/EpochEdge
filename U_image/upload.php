08f4b1a377c9f6970f4f8c3199ba145df6abf3a3
Arijul007 <arijul008@gmail.com>
2024-01-29 20:11:50 +0530

    add_new_part

---
+++ U_image/upload.php
@@ -0,0 +1,50 @@
+<?php
+    if (isset($_POST['upload'])) 
+    {
+        include 'im.connect.php';
+        $images = $_FILES['images'];
+        $num_of_imgs = count($images['name']);
+        # Number of images $num_of_imgs = count($images['name']);
+        
+        for ($i=0; $i < $num_of_imgs; $i++) 
+        {
+            # get the image info and store them in var
+            $image_name = $images['name'][$i];
+            $tmp_name   = $images['tmp_name'][$i];
+            $error      = $images['error'][$i];
+
+            if ($error === 0) 
+            {
+                $img_ex = pathinfo($image_name, PATHINFO_EXTENSION);
+
+                $img_ex_lc = strtolower($img_ex);
+
+                $allowed_exs = array('jpg', 'jpeg', 'png' ,'pdf');
+
+                if (in_array($img_ex_lc, $allowed_exs)) 
+                {
+                    /**renaming the image name with with random string**/
+                    $new_img_name = uniqid('IMG-', true).'.'.$img_ex_lc;
+                    # crating upload path on root directory
+                    $img_upload_path = 'uploads/' .$new_img_name;
+
+                    $sql = "INSERT INTO images (img_name) VALUES (?)";
+                    $stmt = $conn->prepare($sql);
+                    $stmt->execute([$new_img_name]);
+                    # move uploaded image to 'uploads' folder 
+                    move_uploaded_file($tmp_name, $img_upload_path);
+
+                    header("Location: index.php");
+                    
+                }
+                else 
+                {
+                    $em = "You can't upload this type of file";
+
+                    header("Location: index.php?error=$em");
+                }
+          
+            }
+        }
+    }
+?>
