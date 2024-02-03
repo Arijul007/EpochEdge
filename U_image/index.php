d05447523d109f2ab0e9d5c3f412b5e364527596
Arijul007 <arijul008@gmail.com>
2024-02-03 23:15:52 +0530

    change_code

--- U_image/index.php
+++ U_image/index.php
@@ -1,93 +1,35 @@
 <?php
-include '../db_connect.php';
-
-$sName = $_ENV['DB_HOST'];
-$uName = $_ENV['DB_USER'];
-$pass = $_ENV['DB_PASSWORD'];
-$db_name = $_ENV['DB_NAME'];
-
-try 
-{
-    $conn = new PDO("mysql:host=$sName; dbname=$db_name", $uName, $pass);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} 
-catch (PDOException $e) 
-{
-    echo "Connection failed: " . $e->getMessage();
-}
-
-# fetching images
-$sql = "SELECT img_name FROM images ORDER BY id DESC";
-$stmt = $conn->prepare($sql);
-$stmt->execute();
-$images = $stmt->fetchAll();
+    require '../db_connect.php';
 ?>
-
-
-
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Multi Image Upload</title>
-    <style>
-        body 
-        {
-            display: flex;
-            align-items: center;
-            flex-direction: column;
-            font-family: 'Roboto', sans-serif;
-        }
-
-        .error 
-        {
-            color: #a00;
-        }
-        .gallery 
-        {
-            width: 100%;
-            max-width: 600px; /* Set a max-width for the gallery container */
-            margin-top: 20px;
-        }
-        .gallery img 
-        {
-            width: 100%; /* Make images responsive within the gallery */
-            height: auto;
-            margin-bottom: 10px;
-        }
-    </style>
-</head>
-
+<html>
+<head> </head>
 <body>
-    <form method="post" action="upload.php" enctype="multipart/form-data">
-
+    <table border = 1 cellspacing=0 cellpadding= 10>
+        <tr>
+            <td>#</td>
+            <td>Name</td>
+            <td>Image</td>
+        </tr>
         <?php
-        if (isset($_GET['error'])) 
-        {
-            echo "<p class='error'>";
-            echo htmlspecialchars($_GET['error']);
-            echo "</p>";
-        }
+        $i = 1;
+        $rows = mysqli_query($conn, "SELECT * FROM images");
+        foreach($rows as $row) :
         ?>
+        <tr>
+        <td> <?php echo $i++; ?> </td>
+        <td> <?php echo $row["name"]; ?> </td>
+        <td>
+            <?php
 
-        <input type="file" name="images[]" multiple>
-
-
-        <button type="submit" name="upload">Upload</button>
-    </form>
-    <?php if ($stmt->rowCount() > 0) 
-        { 
-    ?>
-            <div class="gallery">
-            <h4>All Images</h4>
-            <?php foreach ($images as $image) { ?>
-                <img src="uploads/<?= $image['img_name'] ?>">
-            <?php } ?>
-        </div>
-    <?php } ?>
-
+            foreach(json_decode($row["image"]) as $image) : 
+            ?>
+            <img src="uploads/<?php echo $image; ?>" width=200>
+             <?php endforeach; ?>
+        </td>
+        </tr>
+        <?php endforeach; ?>
+    </table>
+    <br>
+    <a href="upload.php">Upload Image</a>
 </body>
-
 </html>
