5260d8a044e8e19ae9078261bc6eef254fd91b6b
Arijul007 <arijul008@gmail.com>
2024-02-04 12:53:50 +0530

    U_image_update

--- U_image/index.php
+++ U_image/index.php
@@ -1,34 +1,36 @@
 <?php
-    require '../db_connect.php';
+    include '../db_connect.php';
 ?>
-<html>
-<head> </head>
-<body>
-    <table border = 1 cellspacing=0 cellpadding= 10>
-        <tr>
-            <td>#</td>
-            <td>Name</td>
-            <td>Image</td>
-        </tr>
-        <?php
-        $i = 1;
-        $rows = mysqli_query($conn, "SELECT * FROM images");
-        foreach($rows as $row) :
-        ?>
-        <tr>
-        <td> <?php echo $i++; ?> </td>
-        <td> <?php echo $row["name"]; ?> </td>
-        <td style="display: flex; align-items: center; gap: 10px;">
-            <?php
-            foreach(json_decode($row["image"]) as $image) : 
-            ?>
-            <img src="uploads/<?php echo $image; ?>" width=200>
-             <?php endforeach; ?>
-        </td>
-        </tr>
-        <?php endforeach; ?>
-    </table>
-    <br>
-    <a href="upload.php">Upload Image</a>
-</body>
+
+
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Image Upload</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+  </head>
+  <body>
+    <h1 class="text-center my-3">Registration form</h1>
+    <div class="container d-flex justify-content-center" >
+        <form action="upload.php" method="post" class="w-50" enctype="multipart/form-data">
+            <div class="form-group my-2">
+                <input type="text" name="username"placeholder="Username"class="form-control">
+            </div>
+
+            <div class="form-group my-2">
+                <input type="text" name="email"placeholder="email"class="form-control">
+            </div>
+
+            <div class="form-group my-2">
+                <input type="file" name="file" class="form-control" multiple>
+            </div>
+            
+            <button class="btn btn-dark" type="submit" name="submit">Submit</button>
+        </form>
+    </div>
+    
+  </body>
 </html>
--- U_image/upload.php
+++ U_image/upload.php
@@ -1,49 +1,102 @@
 <?php
-include '../db_connect.php';
+    include '../db_connect.php';
 
-if(isset($_POST["submit"]))
-{
-    $name = $_POST['name'];
-    $totalFiles = count($_FILES['fileImg']['name']);
-    $filesArray = array();
-    
-    for($i = 0; $i < $totalFiles; $i++)
+    if ($_SERVER["REQUEST_METHOD"] == "POST")
     {
-        $imageName = $_FILES["fileImg"]["name"][$i];
-        $tmpName = $_FILES["fileImg"]["tmp_name"][$i];
-        $imageExtension = explode('.', $imageName);
-        $imageExtension = strtolower(end($imageExtension));
-        $newImageName = uniqid() . '.' . $imageExtension;
-        move_uploaded_file($tmpName, 'uploads/'.$newImageName);
-        $filesArray[] = $newImageName;
-    }
+       $username=$_POST['username'];
+       $email=$_POST['email'];
+       $image=$_FILES['file'];
+       echo $username;
+       echo $email; 
+       print_r($image);
+       $imagefilename=$image['name'];
+       $imagefileerror=$image['error'];
+       $imagefiletemp=$image['tmp_name'];
+       $filename_separate=explode('.',$imagefilename);
+       print_r($filename_separate);
+       $file_extension=strtolower(end($filename_separate));
+       print_r($file_extension);
+       $extension=array('jpeg','png','jpg','pdf');
+       if(in_array($file_extension,$extension))
+       {
+            $upload_image='uploads/'.$imagefilename;
+            move_uploaded_file($imagefiletemp,$upload_image);
+            $conn = db_connect();
 
-    $filesArray = json_encode($filesArray);
-    
-    // Assuming $conn is your database connection
-    $query = "INSERT INTO images VALUES('', '$name', '$filesArray')";
-    
-    mysqli_query($conn, $query);
-    
-    echo "
-        <script>
-        alert('Successfully Added');
-        document.location.href = 'index.php';
-        </script>
-    ";
-}
+            $sql = "insert into `images` (Username,email,image) values('$username','$email','$upload_image')";
+            $result=mysqli_query($conn,$sql);
+            if($result)
+            {
+                echo '<div class="alert alert-success" role="alert"><strong>success!</strong> Data inserted successfully </div>';
+            }
+            else
+            {
+                die(mysqli_error($conn));
+            }
+        }
+
+    }
 ?>
-<html>
-<head> </head>
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <style>
+        img{
+            width: 100px;
+        }
+
+    </style>
+</head>
 <body>
-<form enctype="multipart/form-data" action="" method="post">
-    Name :
-    <input type="text" name="name" required> <br>
-    Image :
-    <input type="file" name="fileImg[]" accept=".jpg, .jpeg, .png" required multiple> <br>
-    <button type="submit" name="submit">Submit</button>
-</form>
-<br>
-<a href="index.php">Index</a>
+    
+    <h1 class="text-center my-4">User data</h1>
+    <div class="container mt-5 justify-content-center">
+        <table class="table table-bordered ">
+            <thead class="table-dark">
+                <tr>
+                    <th scope="col">SI no</th>
+                    <th scope="col">Username</th>
+                    <th scope="col">email</th>
+                    <th scope="col">image</th>
+                </tr>
+            </thead>
+            <tbody>
+
+                <?php
+                $conn = db_connect();
+
+                $sql="Select * from `images`";
+                $result=mysqli_query($conn,$sql);
+                while($row=mysqli_fetch_assoc($result))
+                {
+                    $id=$row['id'];
+                    $username=$row['Username'];
+                    $email=$row['email'];
+                    $image=$row['image'];
+                    echo '<tr>
+                            <td>'.$id.'</td>
+                            <td>'.$username.'</td>
+                            <td>'.$email.'</td>
+                            <td><img src='.$image.' /></td>
+                        </tr>';
+                }
+
+
+                ?>
+
+                <table class="table"></table>
+            </tbody>
+        </table>
+    </div>
+    
 </body>
 </html>
